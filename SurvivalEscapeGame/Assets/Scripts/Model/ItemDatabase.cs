6a154211e97dc6447777e905b135787e61891801
RYANLEE-PC\RYAN LEE <drdgvhbh@gmail.com>
2017-03-08 16:24:05 -0500

    asdf

---
+++ SurvivalEscapeGame/Assets/Scripts/Model/ItemDatabase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ItemDatabase : MonoBehaviour {
+    private static string path = Application.streamingAssetsPath + "/data.json";
+    private string jsonString;
+
+
+	// Use this for initialization
+	void Start () {
+        jsonString = File.ReadAllText(path);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ SurvivalEscapeGame/Assets/Scripts/View/ItemMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemMenu : MonoBehaviour {
+    [SerializeField]
+    private GameObject DropDownMenu;
+    private Dropdown DropDown;
+
+    private void Start() {
+        DropDown = DropDownMenu.GetComponent<Dropdown>();
+        DropDown.ClearOptions();
+        List<string> itemNames = new List<string>(Global.ItemNames.Values);
+        itemNames.Sort();
+        DropDown.AddOptions(itemNames);
+    }
+
+    public void UpdateDropDownList() {
+
+    } 
+}
